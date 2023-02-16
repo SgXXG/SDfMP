8d51b53119754262d00b33a058aac8f233222822
Stanislav <72697394+SgXXG@users.noreply.github.com>
2023-02-16 12:52:13 +0300

    lab1

--- Lab1/lib/main.dart
+++ Lab1/lib/main.dart
@@ -1,18 +1,51 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:app_client/blocs/notes_color_cubit.dart';
+import 'package:app_client/blocs/notes_cubit.dart';
+import 'package:app_client/ui/add_note_screen.dart';
+import 'package:app_client/ui/main_screen.dart';
+import 'package:app_client/ui/search_screen.dart';
+import 'package:app_client/ui/show_note_screen.dart';
+import 'package:app_client/ui/theme/app_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
-Future<void> main() async{
+import 'blocs/notes_search_cubit.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(Notes());
+  runApp(const MyApp());
 }
 
-class Notes extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
+    return MultiProvider(
+      providers: [
+        Provider<NotesCubit>(
+          create: (context) => NotesCubit(),
+          dispose: (context, value) => value.close(),
+        ),
+        Provider<NotesColorCubit>(
+          create: (context) => NotesColorCubit(),
+          dispose: (context, value) => value.close(),
+        ),
+        Provider<NotesSearchCubit>(
+          create: (context) => NotesSearchCubit(),
+          dispose: (context, value) => value.close(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: AppTheme.dark,
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const MainScreen(),
+          '/search': (context) => const SearchScreen(),
+          '/add': (context) => NoteAddScreen(),
+          '/show': (context) => ShowNoteScreen(),
+        },
+      ),
     );
   }
 }
